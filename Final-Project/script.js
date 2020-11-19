a6bfe7a0121601ed7a4ef9511e71876050c4b80e
edwindhrmwn <73392558+edwindhrmwn@users.noreply.github.com>
2020-11-20 02:10:10 +0700

    Add files via upload

---
+++ Final-Project/script.js
@@ -0,0 +1,11 @@
+/* Body Mass Index (BMI) adalah cara menghitung berat badan ideal berdasarkan
+tinggi dan berat badan. Berbeda dengan cara perhitungan sebelumnya yang membedakan 
+cara perhitungan pria dan wanita, BMI dibedakan berdasarkan usia. Kalkulator BMI -yang
+menjadi salah satu fitur di aplikasi Halodoc, dapat digunakan oleh seseorang yang sudah
+berusia 20 tahun atau lebih. Bagi mereka yang berusia di bawah 20 tahun, ada cara lain
+untuk menghitungnya*/
+
+function cetak() {
+    var hasil = document.getElementById("hasil").value ;
+    document.getElementById("demo").innerHTML = "<b>" + nama +"</b>";
+}
