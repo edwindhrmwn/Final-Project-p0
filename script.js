3e8b005488d9a301a451dc07e06ee1770693767c
edwindhrmwn <73392558+edwindhrmwn@users.noreply.github.com>
2020-11-20 11:11:07 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -5,7 +5,24 @@ menjadi salah satu fitur di aplikasi Halodoc, dapat digunakan oleh seseorang yan
 berusia 20 tahun atau lebih. Bagi mereka yang berusia di bawah 20 tahun, ada cara lain
 untuk menghitungnya*/
 
-function cetak() {
-    var hasil = document.getElementById("hasil").value ;
-    document.getElementById("demo").innerHTML = "<b>" + nama +"</b>";
+{/* <input type=”text” id=”angkaSatu”>
+    <select id=”op”>
+        <option value=”+”>+</option>
+        <option value=”-”>-</option>
+        <option value=”x”>x</option>
+        <option value=”/”>/</option>
+    </select>
+    <input type=”text” id=”angkaDua”>
+    <input type=”submit” onclick=”kalkulator()” value=”=”>
+    <input type=”text” id=”hasil”> */}
+
+function tampilkan () {
+    var beratBadan = document.getElementById("BB").value
+    var tinggiBadan = document.getElementById("TB").value
+    var ideal = beratBadan / (tinggiBadan * tinggiBadan / 10000)
+    var hasil = ideal.toFixed(2)
+
+    document.getElementById("hasil").innerHTML = hasil
 }
+
+document.getElementById("hasil").innerHTML = hasil
